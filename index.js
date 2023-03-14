838fe623764ea90bc27a3760e018784ebc2df589
Vishal Kumar <vishal.tinu1999@gmail.com>
2023-03-14 10:33:19 +0530

    static serving

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use((req,res,next)=>{
 
 app.use(express.static(path.join(__dirname, "./frontend/build")))
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(
         path.join(__dirname, "./frontend/build/index.html"),
         function (err) {
