cbd559382f1de5fb6324cfbb06f6f97c45cff86d
Vishal Kumar <vishal.tinu1999@gmail.com>
2023-03-16 03:08:17 +0530

    delete place code modified

--- frontend/src/utils/Modal.js
+++ frontend/src/utils/Modal.js
@@ -10,7 +10,7 @@ export default function Modal(props) {
   const handleDelete = async () => {
     try {
       setShowModal(false)
-      await fetch(`${process.env.REACT_APP_BACKEND_URL}/${props.placeId}`, {
+      await fetch(`https://tiny-jade-beetle-hose.cyclic.app/${props.placeId}`, {
         method: "DELETE",
         headers :{
           'Content-Type': 'application/json',
